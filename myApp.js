2c50655e21d53c0cf37fecc4e0c05530c4401db3
m-saudkhan <113919644+m-saudkhan@users.noreply.github.com>
2024-06-24 14:51:56 +0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,25 +1,15 @@
 let express = require('express');
 let app = express();
+let http = require('http');
 
 
 
+const server=http.createServer(app);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+server.listen(8080, () => {
+    console.log("Hello World");
+});
 
 
 
